53bbc3d035179d9b4e7fb1469b14447cb7544cbe
Daniel Fernandes <maildanielalexandre@gmail.com>
2020-09-02 21:24:47 +0100

    Remove LFS

--- README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2c489d52863b9d99c717d0ecf6dbdcfdb4621bd3c4827a639148c578c565917c
-size 126
--- speech_recognition.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7aa342b8ef8bc0f2de75230258acdf9d295d8451e52c6a4fd4013c8f9137367d
-size 2846
